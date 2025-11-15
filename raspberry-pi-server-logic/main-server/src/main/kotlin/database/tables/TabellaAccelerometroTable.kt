84d1c89ae0590fb697d2594b7ac892065623a7d0
Sim0Batt <simonebatt51@gmail.com>
2025-11-15 15:01:58 +0100

    Added sensors templeate logic and folter refactor

--- raspberry-pi-server-logic/main-server/src/main/kotlin/database/tables/TabellaAccelerometroTable.kt
+++ raspberry-pi-server-logic/main-server/src/main/kotlin/database/tables/TabellaAccelerometroTable.kt
@@ -5,5 +5,4 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 object TabellaAccelerometroTable: IntIdTable("Accelerometro") {
     val movimento = bool("movimento")
     val timestamp = varchar("timestamp", 100)
-
 }
---
+++ raspberry-pi-server-logic/raspberry-sensors-logic/electrode_sensor.py
@@ -0,0 +1,11 @@
+#Sensor Logic
+
+
+
+
+
+
+
+#MAIN DON'T TOUCH
+if __name__ == '__main__':
+    print("success")
